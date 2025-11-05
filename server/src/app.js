50cfc8674ba7ec42fe2b23e9b2fbe10b1a1fc748
vijay patidar <vijaypatidar1900@gmail.com>
2025-11-05 18:26:33 +0530

    backend health checkup

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser" 
-
+import mongoose from "mongoose"
 const app = express()
 
 import dotenv from 'dotenv';
@@ -23,6 +23,17 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
 }));
+
+app.get("/api/v1", (req, res) => {
+  const dbStatus = mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+
+  res.status(200).json({
+    success: true,
+    message: "Server is running 🚀",
+    database: dbStatus,
+    time: new Date().toISOString(),
+  });
+});
 
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
