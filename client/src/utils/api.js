011d693aa7054fd3da58ee65d3419e256a093a1d
vijay patidar <vijaypatidar1900@gmail.com>
2025-11-05 20:18:54 +0530

    Update BASE_URL to use VITE_BACKEND_URL in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 // src/utils/api.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
